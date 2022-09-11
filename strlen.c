a18596a2a1c2c1132ff31ab3a68ca9a33b6d6e44
Aman Goswami <97969099+Galvan7@users.noreply.github.com>
2022-09-11 09:28:37 +0530

    Update strlen.c

--- strlen.c
+++ strlen.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<string.h>
 void main(){
-    char x[7]={"shreya"};
+    char x[7]={"aman"};
     int i;
     char temp;
     printf("%d",strlen(x));
-    int l=strlen(x);
-    for(i=0;i<l/2;i++){
+    int k=strlen(x);
+    for(i=0;i<k/2;i++){
         temp=x[i];
-        x[i]=x[l-1-i];
-        x[l-1-i]=temp;
+        x[i]=x[k-1-i];
+        x[k-1-i]=temp;
     }
     printf("%s",x);
 }
